f79ef4ef575c4064e4a1eaa2072face355ffed71
salai <salainimalan@gmail.com>
2025-03-27 13:34:35 +0530

    bb

--- pages/transaction.py
+++ pages/transaction.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 # ✅ Check authentication status
 if "authenticated" not in st.session_state or not st.session_state.authenticated:
     st.warning("⚠️ You must be logged in to access this page.")
@@ -17,6 +17,8 @@ st.write("Here you can access financial data...")
 # Logout button
 if st.button("Logout"):
     st.session_state.authenticated = False
+    af = pd.DataFrame(st.session_state.all_data, columns=st.session_state.column_names)
+    af.merge(st.session_state.df)
     st.warning("⚠️ You have been logged out.")
     st.rerun()
 
